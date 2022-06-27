1d686fb30186b8731aa98c1e4bf6085f48ccfa32
Mateo Chavez <mateochavez424@gmail.com>
2022-06-27 00:14:27 -0300

    Desafío contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './containers/ItemListContainer';
 
--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import ItemCount from '../../components/ItemCount';
+import Swal from 'sweetalert2';
 
 const ItemListContainer = () => {
 
   const onAdd = (count) => {
-      count === 1 ? alert(`Se agrego ${count} al carrito`) : alert(`Se agregaron ${count} al carrito`);
+    Swal.fire({
+      position: 'bottom',
+      width: '32rem',
+      title: `Added ${count} to cart`,
+      showConfirmButton: false,
+      timer: 1500,
+      showClass: {
+          popup: 'animate__animated animate__fadeInUp'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutDown'
+        },
+      customClass: {
+        title: 'fs-6 m-0'
+      }
+    })
   }
 
   return (
-    <div className='bg-black  text-center'>
+    <div className='bg-black text-center'>
       <ItemCount stock={10} initial={1} onAdd={onAdd}/>
     </div>
   )
